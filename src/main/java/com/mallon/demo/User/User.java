4f123e88a3e3f5bf5015e771298c31c134d0748c
maxwellsarpong <maxwellsarpong07@gmail.com>
2021-11-24 11:54:37 +0000

    User class has been created with username, email, password as fields

--- src/main/java/com/mallon/demo/User/User.java
+++ src/main/java/com/mallon/demo/User/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -61,5 +62,20 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && username.equals(user.username) && password.equals(user.password) && email.equals(user.email);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email);
     }
 }
