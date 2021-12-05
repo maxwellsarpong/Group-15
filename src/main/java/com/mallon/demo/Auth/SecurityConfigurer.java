ea6afd815bec5a3c853a6b300f4fe25e00c777d7
maxwellsarpong <maxwellsarpong07@gmail.com>
2021-12-05 14:22:52 +0000

    entity relationships have been resolved, client connectivity started but yet to be finalised, yet to implement strategy to define which exchange to trade on

--- src/main/java/com/mallon/demo/Auth/SecurityConfigurer.java
+++ src/main/java/com/mallon/demo/Auth/SecurityConfigurer.java
@@ -3,14 +3,12 @@ package com.mallon.demo.Auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
